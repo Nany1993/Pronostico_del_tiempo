6be15996a5f23308ab8f22bebfc990016b3f29a7
Nany1993 <anamariagarcia093@gmail.com>
2023-08-29 20:36:32 -0500

    actualización

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
 # Importar módulos necesarios
+from fastapi import FastAPI
+app = FastAPI()
+
 import time
 import requests
 import pandas as pd
-from tqdm import tqdm
 from datetime import datetime
 from twilio_config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, PHONE_NUMBER, API_KEY_WAPI
 from utils import send_twilio_message, get_forecast  # Importar funciones desde utils.py
 
-query = 'Cucuta'
-api_key = API_KEY_WAPI
+@app.get("/")
+def index():
+    return {"mensaje" : "Bienvenidos al Prónostico del tiempo"}
 
-url_clima = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+query+'&days=1&aqi=no&alerts=no'
+@app.get("/clima/{ciudad}/{hora}")
+def clima_ciudad_hora(ciudad: str, hora: int):
+    api_key = API_KEY_WAPI
+    url_clima = f'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={ciudad}&days=1&aqi=no&alerts=no'
+    response = requests.get(url_clima).json()
 
-response = requests.get(url_clima).json()
+    # Filtrar los datos correspondientes a la hora ingresada
+    datos = [get_forecast(response, hora)]
 
-datos = []
-Horas_Disponibles = len(response['forecast']['forecastday'][0]['hour'])
-for i in tqdm(range(Horas_Disponibles), colour='green'):
-    datos.append(get_forecast(response, i))  # Utilizar la función get_forecast desde utils.py
+    col = ['Fecha', 'Hora', 'Condicion', 'Temperatura', 'Lluvia', 'prob_lluvia']
+    df = pd.DataFrame(datos, columns=col)
 
-col = ['Fecha', 'Hora', 'Condicion', 'Temperatura', 'Lluvia', 'prob_lluvia']
-df = pd.DataFrame(datos, columns=col)
-df = df.sort_values(by='Hora', ascending=True)
+    # Convertir el DataFrame a formato JSON
+    result = df.to_dict(orient="records")
+    
+    # Modificar los valores de lluvia para que sean más descriptivos
+    result[0]['Lluvia'] = "Sí lloverá" if result[0]['Lluvia'] == 1 else "No lloverá"
 
-df_rain = df[(df["Hora"] > 6) & (df["Hora"] < 22)]
-df_rain = df_rain[["Hora", "Condicion", "Temperatura"]]
-df_rain.set_index("Hora", inplace=True)
+    # Modificar los valores de probabilidad de lluvia
+    result[0]['prob_lluvia'] = f"{result[0]['prob_lluvia']}% de probabilidad de que llueva"
 
-time.sleep(2)
+    return result
 
-# Crear el cuerpo del mensaje
-message_body = (
-    f'\nHola! \n\n\n El pronostico de lluvia hoy {df["Fecha"][0]} en {query} es :\n\n\n {str(df_rain)}'
-)
 
-# Llamar a la función para enviar el mensaje de Twilio
-send_twilio_message(message_body, '+573005424395')
 
-print('Mensaje Enviado')
+
+"""@app.get("/Ciudad/{ciudad}")
+def pronostico(ciudad):
+    api_key = API_KEY_WAPI
+    url_clima = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+ciudad+'&days=1&aqi=no&alerts=no'
+    response = requests.get(url_clima).json()
+    datos = []
+    Horas_Disponibles = len(response['forecast']['forecastday'][0]['hour'])
+    for i in range(Horas_Disponibles):
+        datos.append(get_forecast(response, i)) 
+
+    col = ['Fecha', 'Hora', 'Condicion', 'Temperatura', 'Lluvia', 'prob_lluvia']
+    df = pd.DataFrame(datos, columns=col)
+    df = df.sort_values(by='Hora', ascending=True)
+    df_rain = df[(df["Hora"] > 6) & (df["Hora"] < 22)]
+    df_rain = df_rain[["Hora", "Condicion", "Temperatura"]]
+    df_rain.set_index("Hora", inplace=True)
+    
+    return df_rain"""
+    
+    
